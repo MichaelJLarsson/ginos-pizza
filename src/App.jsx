b2654fdb7d30ef5292f6c9c4eaab5c9722406cfb
Mikael Larsson <mikael-home@MacBook-Air-som-tillhor-Mikael.local>
2025-06-25 14:16:30 +0200

    Added Header.tsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order.jsx";
 import PizzaOfTheDay from "./PizzaOfTheDay.jsx";
+import Header from "./Header.jsx";
+import { CartContext } from "./context.js";
 
 const App = () => {
+  const cartHook = useState([]);
   return (
     <div>
-      <h1 className="logo">Padre Gino's</h1>
-      <Order />
-      <PizzaOfTheDay />
+      <CartContext.Provider value={cartHook}>
+        <Header />
+        <Order />
+        <PizzaOfTheDay />
+      </CartContext.Provider>
     </div>
   );
 };
--- src/Order.jsx
+++ src/Order.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import Pizza from "./Pizza";
 import Cart from "./Cart";
+import { CartContext } from "./context.js";
 
 const intl = new Intl.NumberFormat("sv-SE", {
   style: "currency",
@@ -13,7 +14,7 @@ export default function Order() {
   const [pizzaSize, setPizzaSize] = useState("M");
   const [pizzaTypes, setPizzaTypes] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useContext(CartContext);
 
   let price, selectedPizza;
 
