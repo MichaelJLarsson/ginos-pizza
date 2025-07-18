4bbd3da088a8a6823decd9d42410286414cc2aa1
Mikael Larsson <mikael.j.larsson@hotmail.com>
2025-07-18 15:16:16 +0200

    Removed artificial delay to make it testable in usePizzaOfTheDay.js

--- src/usePizzaOfTheDay.js
+++ src/usePizzaOfTheDay.js
@@ -3,17 +3,16 @@ import { useState, useEffect, useDebugValue } from "react";
 export const usePizzaOfTheDay = () => {
   const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
 
+  useDebugValue(pizzaOfTheDay ? `${pizzaOfTheDay.name}` : "Loading...");
+
   useEffect(() => {
     async function fetchPizzaOfTheDay() {
-      await new Promise((resolve) => setTimeout(resolve, 3000));
       const response = await fetch("/api/pizza-of-the-day");
       const data = await response.json();
       setPizzaOfTheDay(data);
     }
     fetchPizzaOfTheDay();
   }, []);
-
-  useDebugValue(pizzaOfTheDay ? `${pizzaOfTheDay.name}` : "Loading...");
 
   return pizzaOfTheDay;
 };
