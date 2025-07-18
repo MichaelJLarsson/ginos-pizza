2d19e36ce2070b5c57f42e2e1a568417c3f9277b
Mikael Larsson <mikael.j.larsson@hotmail.com>
2025-07-18 15:22:10 +0200

    Fixed error message in usePizzaOfTheDay.test.jsx

--- src/__tests__/usePizzaOfTheDay.test.jsx
+++ src/__tests__/usePizzaOfTheDay.test.jsx
@@ -15,14 +15,16 @@ const testPizza = {
   image: "/public/pizzas/calabrese.webp",
   sizes: { S: 12.25, M: 16.25, L: 20.25 },
 };
+const jsonTestPizza = JSON.stringify(testPizza);
 
 test("to be null on initial load", async () => {
+  fetch.mockResponseOnce(jsonTestPizza);
   const { result } = renderHook(() => usePizzaOfTheDay(""));
   expect(result.current).toBeNull();
 });
 
 test("To call the API and get the Pizza of the day", async () => {
-  fetch.mockResponseOnce(JSON.stringify(testPizza));
+  fetch.mockResponseOnce(jsonTestPizza);
   const { result } = renderHook(() => usePizzaOfTheDay(""));
   await waitFor(async () => {
     expect(result.current).toEqual(testPizza);
